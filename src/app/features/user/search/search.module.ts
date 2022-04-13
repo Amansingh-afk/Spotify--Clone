5824a962ea88e9fc4f0085fc750b436735a060d5
Aman Singh <amansingh.afk@gmail.com>
2022-04-13 22:04:43 +0530

    gg

--- src/app/features/user/search/search.module.ts
+++ src/app/features/user/search/search.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { SearchRoutingModule } from './search-routing.module';
+import { PageSearchComponent } from './page-search/page-search.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    PageSearchComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    SearchRoutingModule,
+    SharedModule
   ]
 })
 export class SearchModule { }
--- src/app/features/user/user-routing.module.ts
+++ src/app/features/user/user-routing.module.ts
@@ -1,14 +1,35 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PageHomeLayoutComponent } from 'src/app/shared/layout/layout-structures/page-home-layout/page-home-layout.component';
-
+import { PageSearchLayoutComponent } from 'src/app/shared/layout/layout-structures/page-search-layout/page-search-layout.component';
+import { PageLibraryLayoutComponent } from 'src/app/shared/layout/layout-structures/page-library-layout/page-library-layout.component'
 const routes: Routes = [
   {
     path: '',
     component: PageHomeLayoutComponent,
     children: [
       { path: "", redirectTo: "home" },
-      { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) }
+      { 
+        path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) 
+      }
+    ]
+  },
+  {
+    path: '',
+    component: PageSearchLayoutComponent,
+    children: [
+      {
+        path: 'search', loadChildren: () => import('./search/search.module').then(m => m.SearchModule)
+      }
+    ]
+  },
+  {
+    path: '',
+    component: PageLibraryLayoutComponent,
+    children: [
+      {
+        path: 'library', loadChildren: () => import('./library/library.module').then(m => m.LibraryModule)
+      }
     ]
   }
 ];
