26f61ef1719d5fd86fec49b2efe24acdccccf896
Aman Singh <amansingh.afk@gmail.com>
2022-04-14 06:24:52 +0530

    ..

--- src/app/shared/global-components/global-components.module.ts
+++ src/app/shared/global-components/global-components.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { PageErrorComponent } from './page-error/page-error.component';
 import { MusicCardComponent } from './music-card/music-card.component';
+import { PlayBarComponent } from './play-bar/play-bar.component';
 
 
 
 @NgModule({
   declarations: [
     PageErrorComponent,
-    MusicCardComponent
+    MusicCardComponent,
+    PlayBarComponent
   ],
   imports: [
     CommonModule,
---
+++ src/app/shared/global-components/play-bar/play-bar.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PlayBarComponent } from './play-bar.component';
+
+describe('PlayBarComponent', () => {
+  let component: PlayBarComponent;
+  let fixture: ComponentFixture<PlayBarComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ PlayBarComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PlayBarComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
